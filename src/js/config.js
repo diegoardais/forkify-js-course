96b2b570d69ae2e8735c2c1d312b1cc7a74e0885
diegoardais <diegoardais@gmail.com>
2023-02-07 17:38:51 -0300

    Inital commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4c5e6257-baf9-4b66-b6e9-bc36e3cfccaf';
+export const MODAL_CLOSE_SEC = 2.5;
